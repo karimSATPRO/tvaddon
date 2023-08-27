4dcc224fa0ff678e514d76d95e07616147787253
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2023-08-27 18:20:17 +0100

    Update tvadd.sh

--- tvadd.sh
+++ tvadd.sh
@@ -1,1 +1,19 @@
 
+echo "##############################################"
+écho "#############################################" 
+echo "##############################################"
+echo "############ TVADDON_2.1.1 #################"
+
+wget -O /tmp/enigma2-plugin-extensions-tvaddon_2.1.1_all.ipk "https://github.com/karimSATPRO/tvaddon/raw/main/enigma2-plugin-extensions-tvaddon_2.1.1_all.ipk?raw=true"
+
+opkg install --force-overwrite /tmp/*.ipk
+
+echo ""
+cd ..
+sync
+echo "############ Installation Successful  ########"
+echo "############ Restart Enigma2 GUI... #################" 
+init 4
+sleep 2
+init 3
+exit 0
